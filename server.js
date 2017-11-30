ce2453a0a7344949d5cfd4f58ab2bc886d65e069
Jack Woods <jackrwoods@gmail.com>
2017-11-30 12:30:23 -0800

    Add handlebars rendering to server.js

--- server.js
+++ server.js
@@ -1,25 +1,33 @@
+// Startup Procedure
+console.log("== Starting NodeJS Server ==");
+console.log("Require: path");
 var path = require('path');
+console.log("Require: express");
 var express = require('express');
-var exphbs = require('express-handlebars', {defaultLayout: 'main'});
+console.log("Require: express-handlebars");
+var exphbs = require('express-handlebars');
+console.log("Initializing express...");
 var app = express();
 var port = process.env.PORT || 3000;
 
 // Set handlebars as rendering engine
-app.engine('handlebars', exphbs());
+console.log("Set rendering engine...");
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
 app.set('view engine', 'handlebars');
+
+console.log("Done!");
 
 // Serve static files from public
 app.use(express.static('public'));
 
-// Serve handlebars templates
-app.get('index.html', function(req, res) {
-  res.status(200).sendFile(path.join(__dirname, 'public', '404.html'));
+app.get('/', function (req, res, next) {
+
 });
 
 app.get('*', function (req, res) {
-  res.status(404).sendFile(path.join(__dirname, 'public', '404.html'));
+  res.status(404).render("404");
 });
 
 app.listen(port, function () {
-  console.log("== Server is listening on port", port);
+  console.log("== Server is listening on port", port, "==");
 });
