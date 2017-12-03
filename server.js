3a1ab81afdeb8b3b1a25ecf6602d2343a6bbcb55
InjunLee <leeinj@oregonstate.edu>
2017-12-04 05:25:24 +0900

    Add mongodb to server.js

--- server.js
+++ server.js
@@ -1,12 +1,30 @@
 var path = require('path');
 var express = require('express');
 var exphbs = require('express-handlebars', {defaultLayout: 'main'});
+var bodyParser = require('body-parser');
+var MongoClient = require('mongodb').MongoClient;
 var app = express();
 var port = process.env.PORT || 3000;
 
+//mongodb varibles
+var mongoHost = process.env.MONGO_HOST;
+var mongoPort = process.env.MONGO_PORT || 27017;
+var mongoUser = process.env.MONGO_USER;
+var mongoPassword = process.env.MONGO_PASSWORD;
+var mongoDBName = process.env.MONGO_DB;
+
+var mongoURL = 'mongodb://' + mongoUser + ':' + mongoPassword +
+  '@' + mongoHost + ':' + mongoPort + '/' + mongoDBName;
+
+var mongoConnection = null;
+
+
+  
 // Set handlebars as rendering engine
 app.engine('handlebars', exphbs());
 app.set('view engine', 'handlebars');
+
+app.use(bodyParser.json());
 
 // Serve static files from public
 app.use(express.static('public'));
@@ -22,4 +40,14 @@ app.get('*', function (req, res) {
 
 app.listen(port, function () {
   console.log("== Server is listening on port", port);
+});
+
+MongoClient.connect(mongoURL, function (err, connection) {
+  if (err) {
+    throw err;
+  }
+  mongoConnection = connection;
+  app.listen(port, function () {
+    console.log("== Server listening on port:", port);
+  });
 });
