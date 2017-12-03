fd65f1502d4c5d18d91b665e768cca8cbc680933
Jack Woods <jackrwoods@gmail.com>
2017-12-03 12:14:19 -0800

    Upload modified paintapp.js that included save/load functionality.

--- public/js/paintapp.js
+++ public/js/paintapp.js
@@ -123,7 +123,7 @@ controls.brushSize = function(cx) {
 	var select = elt("select");
 	var sizes = [1, 2, 3, 5, 8, 12, 25, 35, 50, 75, 100];
 	sizes.forEach(function(size) {
-		select.appendChild(elt("option", {value: size}, size + "pixels"));
+		select.appendChild(elt("option", {value: size}, size + " Pixels"));
 	});
 	select.addEventListener("change", function() {
 		cx.lineWidth = select.value;
